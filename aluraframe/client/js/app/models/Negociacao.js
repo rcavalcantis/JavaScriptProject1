6f458a9a0d3145ecbfc08dc2dc0e1d2711f0288b
Rodrigo Cavalcanti <rodrigo.cavalcanti-silva@gmail.com>
2020-05-27 15:17:26 -0300

    Aula 4 fim

--- aluraframe/client/js/app/models/Negociacao.js
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -15,7 +15,7 @@ class Negociacao {
     
     get data() {
         
-        return new Date(this._data.getTime());
+        return DateHelper.dataParaTexto(this._data.getTime());
     }
     
     get quantidade() {
